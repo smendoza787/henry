73589b26cf36a888240d6be0c8d75edfeddc37eb
Sergio Mendoza <sergiomendoza@AULT26.local>
2019-09-28 01:57:06 -0600

    add tests

--- src/components/Title/index.spec.tsx
+++ src/components/Title/index.spec.tsx
@@ -6,6 +6,12 @@ beforeEach(cleanup)
 
 describe('<Title />', () => {
   it('should render', () => {
-    render(<Title title="pop" />)
+    render(<Title title="Example 1" />)
+  })
+
+  it('should render title prop passed to it', () => {
+    const title = "Example 2"
+    const { getByText } = render(<Title title={title} />)
+    getByText(title)
   })
 });
